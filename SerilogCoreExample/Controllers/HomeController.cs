3522e8fb287f3ef43d331df42c2b18a22c41c933
DESKTOP-15VQ6VC\Mariia Suvalova <mariasuvalova96@gmail.com>
2018-03-20 09:44:29 +0200

    Fix solution.

---
+++ SerilogCoreExample/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogTrace("Sample trace message");
+            _logger.LogDebug("Sample debug message");
+            _logger.LogInformation("Sample informational message");
+            _logger.LogWarning("Sample warning message");
+
+            try
+            {
+                throw new ArgumentNullException();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Sample error message");
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
